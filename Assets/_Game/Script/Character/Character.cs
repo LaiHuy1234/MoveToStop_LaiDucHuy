09c69a4ce48c17f661fcbe56deb46e8ca4974651
CaosDev1 <vhoang0327@gmail.com>
2024-07-01 21:12:24 +0700

    +2Init

--- Assets/_Game/Script/Character/Character.cs
+++ Assets/_Game/Script/Character/Character.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Character : MonoBehaviour
 {
     [SerializeField] private LayerMask groundLayer;
-    List<Character> enemy = new List<Character>();
+    public Vector3 target;
+
+
+    public List<Character> targetList = new List<Character>();
+
+
     //check diem tiep theo co phai ground khong(1: tra ve vi tri tiep theo; 0: tra ve vi tri hien tai)
     public bool CheckGround(Vector3 nextPoint)
     {
@@ -13,5 +20,29 @@ public class Character : MonoBehaviour
         return Physics.Raycast(nextPoint, Vector3.down, out hit, 1.5f, groundLayer);
     }
 
+    public void AddTarget(Character target)
+    {
+        if (targetList.Contains(target)) return;    
+        targetList.Add(target);
+    }
 
+    public void RemoveTarget(Character target)
+    {
+        if (!targetList.Contains(target)) return;
+        targetList.Remove(target);
+    }
+
+    public void Attack()
+    {
+        if (targetList.Count > 0) return;
+        {
+            Character char1 = targetList[0];
+            char1.transform.position = target;
+        }
+    }
+
+    public void SetTarget(Vector3 nextPoint)
+    {
+        target = nextPoint;
+    }
 }
--- Assets/_Game/Script/Character/RangerAttack.cs
+++ Assets/_Game/Script/Character/RangerAttack.cs
@@ -1,21 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class RangerAttack : Character
+public class RangerAttack : MonoBehaviour
 {
+
     [SerializeField] private Character owner;
+    [SerializeField] private Weapon weaponPrefab;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
-        {
-            //if(enemy.Count)
-        }
+        AddTargetToTargetList(other);
+        AttackTarget(other);
+        
     }
+
+    private void AddTargetToTargetList(Collider other)
+    {
+        if (!other.CompareTag(Constants.TAG_CHARACTER)) return;
+        Character target1 = other.GetComponent<Character>();
+        owner.AddTarget(target1);
+    }
+
+    private void AttackTarget(Collider other)
+    {
+        Character target = other.GetComponent<Character>();
+        target.Attack();
+    }
+
 
     private void OnTriggerExit(Collider other)
     {
-        
+        RemoveTargetFromTargetList(other);
+    }
+
+    private void RemoveTargetFromTargetList(Collider other)
+    {
+        if (!other.CompareTag(Constants.TAG_CHARACTER)) return;
+        Character target1 = other.GetComponent<Character>();
+        owner.RemoveTarget(target1);
     }
 }
